eb3032814f0e2e02745465ca47e78e934ee8116f
Gaurav kumar <58880601+GAURAV-KUMAR22@users.noreply.github.com>
2025-05-13 13:14:34 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -41,10 +41,7 @@ function shouldCompress(req, res) {
   return compression.filter(req, res);
 }
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://hilarious-mermaid-cc36ae.netlify.app/",
-];
+const allowedOrigins = process.env.FRONTEND;
 app.use(
   cors({
     origin: function (origin, callback) {
