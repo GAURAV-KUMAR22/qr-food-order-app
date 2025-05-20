79bd7725d421cccdcadfdfe6b45f4545fcceb5d8
GAURAV-KUMAR22 <gorav.panwar99@gmail.com>
2025-05-20 15:30:02 +0530

    add product detils page

--- qr-app/src/Pages/Clients/ProductsDetails.jsx
+++ qr-app/src/Pages/Clients/ProductsDetails.jsx
@@ -8,44 +8,50 @@ import { useAuth } from "../../../Context/AuthProvider";
 
 export const ProductsDetails = () => {
   const location = useLocation();
-  const product = location.state.item;
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const product = location.state?.product;
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cartItems);
-  console.log(cart);
   function handleAddtoCart(product) {
     if (cart._id === product._id) {
       alert("Product Already Added");
     }
     dispatch(addToCart(product));
   }
+  console.log(product);
 
   const { isAuthenticated } = useAuth();
+
+  const BeackendUrl =
+    import.meta.env.VITE_MODE === "Production"
+      ? import.meta.env.VITE_BACKEND_PROD
+      : import.meta.env.VITE_BACKEND_DEV;
   return (
     <>
-      <div className="w-[100%] h-[58px] flex items-center">
+      <div className="w-[98%] h-[58px] flex items-center">
         {isAuthenticated ? (
           <ReverseButton route={"/admin"} routeName={"Home"} css={"ml-2"} />
         ) : (
           <ReverseButton route={"/"} routeName={"Home"} css={"ml-2"} />
         )}
       </div>
-      <div className="flex flex-col max-w-[375px] w-[375px] mx-auto ">
-        <div className="rounded-2xl mx-1 mt-6 shadow-lg bg-white p-4  max-w-[365px] w-[365px] overflow-scroll">
+      <div className="flex flex-col w-[98%] sm:w-[50%] mx-auto">
+        <div className="rounded-2xl mx-1 mt-6 shadow-lg bg-white p-4  w-[365px] ">
           {/* Image Section */}
           <div className="flex justify-center items-center">
             <img
-              src={`${backendUrl}/${product.imageUrl}`}
+              src={`${BeackendUrl}/${product.imageUrl}`}
               alt="image"
-              className="w-[250px] h-[200px] object-contain my-3 rounded-full shadow-md"
+              className="w-[40%] h-[180px] object-cover my-3 rounded-full shadow-md"
               style={{ width: "100px", height: "100px" }}
             />
           </div>
 
           {/* Product Details */}
-          <div className=" mt-6 w-full justify-items-start">
-            <h1 className="text-2xl font-bold text-gray-800">
-              <span className="font-light text-gray-500 mr-2">Name:</span>
+          <div className=" mt-6 w-[98%] justify-items-start capitalize">
+            <h1 className="text-2xl font-bold text-gray-800 capitalize">
+              <span className="font-light text-gray-500 mr-2 capitalize">
+                Name:
+              </span>
               {product.name}
             </h1>
             <p className="text-xl text-gray-700 flex justify-center  ">
@@ -64,7 +70,7 @@ export const ProductsDetails = () => {
           </div>
 
           {/* Add to Cart Button */}
-          {admin ? null : (
+          {isAuthenticated ? null : (
             <div className="mt-6 flex justify-center">
               <button
                 onClick={handleAddtoCart}
